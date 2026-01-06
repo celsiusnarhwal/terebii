4ed8c310b996a78ee99e008360f3c7f333df67cc
celsius narhwal <hello@celsiusnarhwal.dev>
2026-01-05 19:10:34 -0500

    Update utils.py

--- terebii/utils.py
+++ terebii/utils.py
@@ -3,7 +3,7 @@ import httpx
 from terebii.settings import settings
 
 
-def sonarr():
+def sonarr() -> httpx.AsyncClient:
     return httpx.AsyncClient(
         base_url=settings().sonarr_url.encoded_string().rstrip("/") + "/api/v3",
         headers={"X-Api-Key": settings().sonarr_api_key.get_secret_value()},
